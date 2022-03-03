d0f5d414776880e6efa7eeee917c27996ef735a8
Matt-Not <70361629+Matt-Not@users.noreply.github.com>
2022-03-03 20:07:57 +0100

    Push by token example

--- app/src/main/java/com/example/didproject/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/didproject/ui/gallery/GalleryViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class GalleryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Samuele Modifica per vedere se funge commit, anche Matteo per lo stesso motivo"
+        value = "This is gallery Samuele Modifica per vedere se funge commit, anche Matteo per lo stesso motivo, prova"
     }
     val text: LiveData<String> = _text
 }
